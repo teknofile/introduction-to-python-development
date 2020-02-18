74eb0af2cd79a52bbbf919d7b22e9db8c37e162e
James Richardson <teknofile@teknofile.org>
2020-02-18 15:07:31 -0700

    added lessons

---
+++ Lecture: Booleans and None/booleans_and_none.py
@@ -0,0 +1,11 @@
+None    # The only thing that is ever None, is None
+True    # != None
+False   # != None
+
+bool(1.0) # True
+
+bool(0) #False
+
+# A "None" string is false, if it has ANYTHING in it, it's true
+bool("") # False
+bool(" ") # True
