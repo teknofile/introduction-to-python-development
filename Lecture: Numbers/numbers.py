17d0590501b182d2d5220fb2697aad87d662e91b
James Richardson <teknofile@teknofile.org>
2020-02-18 15:07:46 -0700

    updated

---
+++ Lecture: Numbers/numbers.py
@@ -0,0 +1,17 @@
+2+2 # = 4
+10 - 4 # =6
+
+3*9 # = 27
+
+5 / 3 # = 1.66666666667
+5 // 3 # = 1
+5 % 3 # = 2
+
+2 ** 3 # 8 # (exponent)
+pow(2, 3) # same as above
+
+# If you do math with a float, your answer will always be a float.
+# Doesn't matter if you do 4.0 + 3... answer will be 7.0 (a float)
+
+str(7.0) # Turns the float 7.0 into a string .. typecast it...
+
