c79d3bf4dc40038b024f7915dc7531ea2b7aa3d5
James Richardson <teknofile@teknofile.org>
2020-02-18 14:43:27 -0700

    added strings

---
+++ Lecutre: Strings/strings.py
@@ -0,0 +1,29 @@
+single_quoted_string = 'single quoted'
+double_quoted_string = "double quoted"
+
+multi_line_string = '''This is a 
+multi line
+string.
+'''
+
+concat_strings = 'single' + ' quote'
+
+multiplication = "ha" * 4 # "hahahaha"
+
+division = "ha" / 4 # Invalid
+
+'single'.find('d') # = -1 doesn't show
+'single'.find('g') # = 3 in position 3 (0-based)
+'single'.find('le') # = 4
+
+"TeStIng".lower() # = "testing"
+
+# Strings are immutable.
+
+print("Tab\tDelimited")
+print("New\nLine")
+print("Slahes\\Character")
+print('"Double" quotes')
+print('\'Encapsulated\' quotes')
+
+
